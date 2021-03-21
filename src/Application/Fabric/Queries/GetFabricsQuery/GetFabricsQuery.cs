b856b6ceee290acfcf6abecd046d379dbe442d35
Mo <mo@it0.me>
2021-03-21 17:03:58 +0100

    add fabrics dto

--- src/Application/Fabric/Queries/GetFabricsQuery/GetFabricsQuery.cs
+++ src/Application/Fabric/Queries/GetFabricsQuery/GetFabricsQuery.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,18 +11,22 @@ using System.Threading.Tasks;
 using TailorStore.Application.Common.Interfaces;
 
 namespace TailorStore.Application.Fabric.Queries.GetFabricsQuery {
-    public class GetFabricsQuery : IRequest<IList<KeyValuePair<Guid, string>>> { }
+    public class GetFabricsQuery : IRequest<IList<GetFabricsQueryDto>> { }
 
-    public class GetFabricsQueryHandler : IRequestHandler<GetFabricsQuery, IList<KeyValuePair<Guid, string>>> {
+    public class GetFabricsQueryHandler : IRequestHandler<GetFabricsQuery, IList<GetFabricsQueryDto>> {
         private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
 
-        public GetFabricsQueryHandler(IApplicationDbContext context) {
+        public GetFabricsQueryHandler(
+            IApplicationDbContext context,
+            IMapper mapper) {
             _context = context;
+            _mapper = mapper;
         }
 
-        public async Task<IList<KeyValuePair<Guid, string>>> Handle(GetFabricsQuery request, CancellationToken cancellationToken) {
+        public async Task<IList<GetFabricsQueryDto>> Handle(GetFabricsQuery request, CancellationToken cancellationToken) {
             return await _context.Fabrics
-                .Select(s => new KeyValuePair<Guid, string>(s.Id, s.Name))
+                .ProjectTo<GetFabricsQueryDto>(_mapper.ConfigurationProvider)
                 .ToListAsync(cancellationToken);
         }
     }
---
+++ src/Application/Fabric/Queries/GetFabricsQuery/GetFabricsQueryDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TailorStore.Application.Common.Mappings;
+
+namespace TailorStore.Application.Fabric.Queries.GetFabricsQuery {
+    public class GetFabricsQueryDto : IMapFrom<Domain.Entities.Fabric> {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+
+        public void Mapping(Profile profile) {
+            profile.CreateMap<Domain.Entities.Fabric, GetFabricsQueryDto>(MemberList.Destination);
+        }
+    }
+}
