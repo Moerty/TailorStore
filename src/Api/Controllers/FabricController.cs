303cfad028954aff4046229fe7a94bf91d69b6f8
Mo <mo@it0.me>
2021-04-09 21:01:39 +0200

    from form

--- src/Api/Controllers/FabricController.cs
+++ src/Api/Controllers/FabricController.cs
@@ -3,9 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TailorStore.Application.Fabric.Commands.AddFabricCommand;
 using TailorStore.Application.Fabric.Commands.DeleteFabricCommand;
+using TailorStore.Application.Fabric.Commands.UpdateFabricCommand;
 using TailorStore.Application.Fabric.Queries.GetFabricsQuery;
 
 namespace Api.Controllers
@@ -17,17 +19,35 @@ namespace Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> PostAsync([FromBody] AddFabricCommand command) {
-            var result = await Mediator.Send(command).ConfigureAwait(false);
+        public async Task<IActionResult> PostAsync(
+            [FromForm] AddFabricCommand command,
+            CancellationToken cancellationToken) {
+            var result = await Mediator
+                .Send(command, cancellationToken)
+                .ConfigureAwait(false);
 
             if (result) return Ok();
             else return Problem();
         }
 
         [HttpDelete]
-        public async Task<IActionResult> DeleteAsync([FromBody] DeleteFabricCommand command) {
+        public async Task<IActionResult> DeleteAsync(
+            [FromBody] DeleteFabricCommand command,
+            CancellationToken cancellationToken) {
             var result = await Mediator
-                .Send(command)
+                .Send(command, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (result) return Ok();
+            else return Problem();
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> PatchFabricAsync(
+            [FromForm] UpdateFabricCommand command,
+            CancellationToken cancellationToken) {
+            var result = await Mediator
+                .Send(command, cancellationToken)
                 .ConfigureAwait(false);
 
             if (result) return Ok();
---
+++ src/Application/Fabric/Commands/UpdateFabricCommand/UpdateFabricCommand.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TailorStore.Application.Common.Interfaces;
+
+namespace TailorStore.Application.Fabric.Commands.UpdateFabricCommand
+{
+    public class UpdateFabricCommand : IRequest<bool>
+    {
+        public Guid Fabricid { get; set; }
+
+#nullable enable
+        public string? FabricName { get; set; }
+        public IFormFile? Picture { get; set; }
+#nullable disable
+    }
+
+    public class UpdateFabricCommandHandler : IRequestHandler<UpdateFabricCommand, bool> {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateFabricCommandHandler(IApplicationDbContext context) {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(UpdateFabricCommand request, CancellationToken cancellationToken) {
+            var entity = await _context.Fabrics
+                .FirstOrDefaultAsync(f => f.Id == request.Fabricid, cancellationToken)
+                .ConfigureAwait(false);
+
+            if(!string.IsNullOrEmpty(request.FabricName)) {
+                entity.Name = request.FabricName;
+            }
+
+            if(request.Picture.Length > 0) {
+                // implement picture
+            }
+
+            return await _context.SaveChangesAsync(cancellationToken).ConfigureAwait(false) > 0;
+        }
+    }
+}
